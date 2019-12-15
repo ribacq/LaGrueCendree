a1092d0b00915c00b7ab2ca1a1c6b9288b2e87b4
Quentin RIBAC <ribac.quentin@gmail.com>
2019-12-15 17:24:12 +0100

    first islands

--- squaresInSpace.go
+++ squaresInSpace.go
@@ -5,15 +5,16 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
 const (
-	GRID_WIDTH  int = 640
-	GRID_HEIGHT int = 360
-	FRAMES      int = 50
+	GRID_WIDTH  int = 1280
+	GRID_HEIGHT int = 720
 	DIAGONAL    int = GRID_WIDTH + GRID_HEIGHT
-	MAX_DIST    int = 32
+	MAX_DIST    int = 128
+	FRAMES      int = MAX_DIST
 	MAX_VAL     int = MAX_DIST
 	START_VAL   int = MAX_VAL
 	RENEW       int = MAX_DIST
@@ -73,34 +74,23 @@ func main() {
 	var squares [GRID_HEIGHT][GRID_WIDTH]int
 	rand.Seed(time.Now().UnixNano())
 
-	println(GRID_WIDTH, "x", GRID_HEIGHT)
+	println(GRID_WIDTH, "x", GRID_HEIGHT, "f", FRAMES)
 
 	// main loop
+	var wg sync.WaitGroup
 	for frame := 0; frame < FRAMES; frame++ {
 		// loop on squares
 		for y := range squares {
 			print("              \r", frame, " ", y)
 			for x := range squares[y] {
+				wg.Add(1)
 				go func(y, x int) {
+					defer wg.Done()
 					// life creation
 					if rand.Intn(RENEW) < 1 {
-						if squares[y][x] == EMPTY {
+						if squares[y][x] <= EMPTY {
 							squares[y][x] = START_VAL
-						} else if squares[y][x] < EMPTY {
-							squares[y][x]++
 						}
-					}
-
-					// next position
-					var nextY, nextX int
-
-					// skip empty
-					if squares[y][x] < EMPTY {
-						squares[y][x]++
-					}
-
-					if squares[y][x] <= EMPTY {
-						return
 					}
 
 					// move to other
@@ -108,30 +98,29 @@ func main() {
 					dist := squares[y][x]
 					for yo := y - dist; yo <= y+dist; yo++ {
 						for xo := x - dist + Abs(yo-y); xo <= x+dist-Abs(yo-y); xo++ {
-							// skip empty squares
 							inyo, inxo := Inside(yo, xo)
 							if squares[inyo][inxo] <= EMPTY {
 								continue
 							}
-
-							// count force
 							dy += (yo - y)
 							dx += (xo - x)
 						}
 					}
-					dx = Sign(dx)
 					dy = Sign(dy)
-					nextY, nextX = Inside(y+dy, x+dx)
-					if (nextY != y || nextX != x) && squares[nextY][nextX] <= EMPTY {
-						squares[nextY][nextX] = squares[y][x]
-						squares[y][x] = -squares[y][x] / 4
+					dx = Sign(dx)
+					if rand.Intn(2) < 1 {
+						dy = 0
+					} else {
+						dx = 0
 					}
-
-					// stay still: slowly die
-					squares[y][x]--
+					nextY, nextX := Inside(y+dy, x+dx)
+					if (nextY != y || nextX != x) && squares[nextY][nextX]*squares[y][x] <= 0 {
+						squares[nextY][nextX], squares[y][x] = squares[y][x]-1, squares[nextY][nextX]-1
+					}
 				}(y, x)
 			}
 		}
+		wg.Wait()
 	}
 
 	// display
@@ -139,12 +128,10 @@ func main() {
 	fmt.Println(GRID_WIDTH, GRID_HEIGHT, MAX_VAL-1)
 	for y := range squares {
 		for x := range squares[y] {
-			if squares[y][x] == EMPTY {
-				fmt.Print(0, 0, 0, " ")
-			} else if squares[y][x] < EMPTY {
+			if squares[y][x] <= EMPTY {
 				fmt.Print(0, 0, -squares[y][x], " ")
-			} else {
-				fmt.Print(0, MAX_VAL-squares[y][x], 0, " ")
+			} else if squares[y][x] > EMPTY {
+				fmt.Print(rand.Intn(1+MAX_VAL-squares[y][x]), MAX_VAL-squares[y][x], 0, " ")
 			}
 		}
 		fmt.Println()
