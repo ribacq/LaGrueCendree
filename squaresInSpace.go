2522c5fcca3c8a2d57ae919d49f43455fe953a3c
Quentin RIBAC <ribac.quentin@gmail.com>
2019-12-17 01:34:46 +0100

    isles in one resolution

--- squaresInSpace.go
+++ squaresInSpace.go
@@ -10,23 +10,28 @@ import (
 )
 
 const (
-	GRID_WIDTH  int = 256
-	GRID_HEIGHT int = 144
+	GRID_WIDTH  int = 1 + 144
+	GRID_HEIGHT int = 1 + 144
 	DIAGONAL    int = GRID_WIDTH + GRID_HEIGHT
-	MAX_DIST    int = 32
-	FRAMES      int = MAX_DIST
-	MAX_VAL     int = MAX_DIST
-	START_VAL   int = MAX_VAL
-	RENEW       int = MAX_DIST
-	EMPTY       int = 0
+	SURFACE     int = GRID_WIDTH * GRID_HEIGHT
 )
 
 var (
-	DIRECTIONS [4][2]int = [4][2]int{
+	MAX_DIST    int = DIAGONAL / 9
+	FRAMES      int = MAX_DIST
+	MAX_VAL     int = MAX_DIST
+	SPAWNS      int = 1
+	SPAWN_POWER int = MAX_DIST
+	SPAWN_DIST  int = MAX_DIST
+)
+
+var (
+	DIRECTIONS [8][2]int = [8][2]int{
 		{0, 1},
 		{0, -1},
 		{1, 0},
 		{-1, 0},
+		{-1, -1}, {-1, 1}, {1, -1}, {1, 1},
 	}
 )
 
@@ -76,76 +81,129 @@ func main() {
 
 	println(GRID_WIDTH, "x", GRID_HEIGHT, "f", FRAMES)
 
+	// spawns
+	spawns := make([]struct{ Y, X int }, SPAWNS)
+	for i := range spawns {
+		spawns[i].Y = rand.Intn(GRID_HEIGHT)
+		spawns[i].X = rand.Intn(GRID_WIDTH)
+	}
+	for _, sp := range spawns {
+		for i := 0; i < SPAWN_POWER; i++ {
+			offY, offX := rand.Intn(2*SPAWN_DIST)-SPAWN_DIST, rand.Intn(2*SPAWN_DIST)-SPAWN_DIST
+			newY, newX := Inside(sp.Y+offY, sp.X+offX)
+			for squares[newY][newX] != 0 || offY*offY+offX*offX > SPAWN_DIST*SPAWN_DIST {
+				offY, offX = rand.Intn(2*SPAWN_DIST)-SPAWN_DIST, rand.Intn(2*SPAWN_DIST)-SPAWN_DIST
+				newY, newX = Inside(sp.Y+offY, sp.X+offX)
+			}
+			if rand.Intn(2) < 1 {
+				squares[newY][newX] = MAX_VAL
+			} else {
+				squares[newY][newX] = -1
+			}
+		}
+	}
+
 	// main loop
 	var wg sync.WaitGroup
 	for frame := 0; frame < FRAMES; frame++ {
+
 		// loop on squares
-		for y := range squares {
+		var seenY [GRID_HEIGHT]bool
+		for range squares {
+			y := rand.Intn(GRID_HEIGHT)
+			for seenY[y] {
+				y = rand.Intn(GRID_HEIGHT)
+			}
+			seenY[y] = true
 			print("              \r", frame, " ", y)
-			for x := range squares[y] {
+			var seenX [GRID_WIDTH]bool
+			for range squares[y] {
+				x := rand.Intn(GRID_WIDTH)
+				for seenX[x] {
+					x = rand.Intn(GRID_WIDTH)
+				}
+				seenX[x] = true
 				wg.Add(1)
 				go func(y, x int) {
 					defer wg.Done()
-					// life creation
-					if rand.Intn(RENEW) < 1 && squares[y][x] <= EMPTY {
-						squares[y][x] = START_VAL - (frame * MAX_VAL / FRAMES)
-					}
 
 					// skip empty
-					if squares[y][x] <= EMPTY {
+					if squares[y][x] == 0 {
 						return
+					}
+
+					if squares[y][x] <= -MAX_VAL {
+						squares[y][x] = 1 - MAX_VAL
 					}
 
 					// move to other
 					dy, dx := 0, 0
-					dist := squares[y][x]
+					dist := (squares[y][x] + MAX_VAL) % MAX_VAL
 					for yo := y - dist; yo <= y+dist; yo++ {
 						for xo := x - dist; xo <= x+dist; xo++ {
 							// yo, xo inside
 							inyo, inxo := Inside(yo, xo)
 
-							// skip empty
-							if squares[inyo][inxo] <= EMPTY {
+							// skip empty and far away
+							if squares[inyo][inxo]*squares[y][x] <= 0 || (yo-y)*(yo-y)+(xo-x)*(xo-x) > dist*dist {
 								continue
 							}
 
 							// count force
-							dy += yo - y
-							dx += xo - x
+							dy += (yo - y)
+							dx += (xo - x)
 						}
 					}
-					dy /= dist
-					dx /= dist
+					if dist > 0 {
+						dy /= dist
+						dx /= dist
+					}
 					nextY, nextX := Inside(y+dy, x+dx)
-					for (dy != 0 || dx != 0) && squares[nextY][nextX] > EMPTY {
-						dy -= Sign(dy)
-						dx -= Sign(dx)
+					ry, rx := Abs(dy), Abs(dx)
+					for (dy != 0 || dx != 0) && squares[nextY][nextX]*squares[y][x] > 0 {
+						if rand.Intn(ry+rx) < ry {
+							dy -= Sign(dy)
+						} else {
+							dx -= Sign(dx)
+						}
 						nextY, nextX = Inside(y+dy, x+dx)
 					}
-					if (nextY != y || nextX != x) && squares[nextY][nextX] <= EMPTY {
-						squares[nextY][nextX], squares[y][x] = squares[y][x]-1, squares[nextY][nextX]-1
-						return
-					}
-
-					squares[y][x]++
-					if squares[y][x] >= MAX_VAL {
-						squares[y][x] = MAX_VAL - 1
+					if squares[nextY][nextX]*squares[y][x] <= 0 {
+						squares[nextY][nextX], squares[y][x] = squares[y][x]-1, squares[nextY][nextX]-Sign(squares[y][x])
+					} else if ry == 0 && rx == 0 {
+						dir := rand.Intn(len(DIRECTIONS))
+						nextY, nextX = Inside(y+DIRECTIONS[dir][0], x+DIRECTIONS[dir][1])
+						i := 0
+						for i < len(DIRECTIONS) && squares[nextY][nextX]*squares[y][x] > 0 {
+							dir = (dir + 1) % len(DIRECTIONS)
+							nextY, nextX = Inside(y+DIRECTIONS[dir][0], x+DIRECTIONS[dir][1])
+							i++
+						}
+						if i < len(DIRECTIONS) {
+							squares[nextY][nextX], squares[y][x] = squares[y][x]-1, squares[nextY][nextX]-Sign(squares[y][x])
+						}
 					}
 				}(y, x)
 			}
 		}
+		wg.Wait()
 	}
-	wg.Wait()
 
 	// display
 	fmt.Println("P3")
-	fmt.Println(GRID_WIDTH, GRID_HEIGHT, MAX_VAL-1)
+	fmt.Println(GRID_WIDTH-1, GRID_HEIGHT-1, MAX_VAL-1)
 	for y := range squares {
 		for x := range squares[y] {
-			if squares[y][x] <= EMPTY {
-				fmt.Print(0, 0, -squares[y][x], " ")
-			} else if squares[y][x] > EMPTY {
-				fmt.Print(rand.Intn(1+MAX_VAL-squares[y][x]), MAX_VAL-squares[y][x], 0, " ")
+			val := squares[y][x]
+			if y < GRID_HEIGHT-1 && x < GRID_WIDTH-1 {
+				val = (squares[y][x] + squares[y+1][x] + squares[y][x+1] + squares[y+1][x+1]) / 4
+			} else {
+				continue
+			}
+			if val <= 0 {
+				fmt.Print(0, 0, -val, " ")
+			} else if val > 0 {
+				fmt.Print(rand.Intn(val), val, 0, " ")
 			}
 		}
 		fmt.Println()
